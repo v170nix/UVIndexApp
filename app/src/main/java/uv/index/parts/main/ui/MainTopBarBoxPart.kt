520fd6cfbd5aeeb396891f1a9c247dab541496f2
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-08-10 19:21:13 +0300

    add animated top bar part

--- app/src/main/java/uv/index/parts/main/ui/MainTopBarBoxPart.kt
+++ app/src/main/java/uv/index/parts/main/ui/MainTopBarBoxPart.kt
@@ -10,77 +10,52 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import uv.index.ui.theme.UVIndexAppTheme
 
 @Stable
-interface MainTopBarColors {
+interface MainTopBarTextStyles {
     @Composable
-    fun placeTextColor(colorTransitionFraction: Float): State<Color>
+    fun placeTextStyle(fraction: Float): State<TextStyle>
 
     @Composable
-    fun titleTextColor(colorTransitionFraction: Float): State<Color>
+    fun titleTextStyle(fraction: Float): State<TextStyle>
 
     @Composable
-    fun indexTextColor(colorTransitionFraction: Float): State<Color>
-}
-
-@Stable
-interface MainTopBarTextSizes {
-    @Composable
-    fun titleTextSize(textTransitionFraction: Float): State<TextUnit>
-
-    @Composable
-    fun indexTextSize(textTransitionFraction: Float): State<TextUnit>
+    fun indexTextStyle(fraction: Float): State<TextStyle>
 }
 
 object MainTopBarDefaults {
-
     @Composable
-    fun mainTopBarColors(
-        placeExpandedColor: Color,
-        placeCollapsedColor: Color = placeExpandedColor,
-        titleExpandedColor: Color,
-        titleCollapsedColor: Color = titleExpandedColor,
-        indexExpandedColor: Color,
-        indexCollapsedColor: Color = indexExpandedColor
-
-    ): MainTopBarColors {
+    fun mainTopBarTextStyles(
+        placeExpandedStyle: TextStyle,
+        placeCollapsedStyle: TextStyle,
+        titleExpandedStyle: TextStyle,
+        titleCollapsedStyle: TextStyle,
+        indexExpandedStyle: TextStyle,
+        indexCollapsedStyle: TextStyle
+    ): MainTopBarTextStyles {
         return remember(
-            placeExpandedColor,
-            placeCollapsedColor,
-            titleExpandedColor,
-            titleCollapsedColor,
-            indexExpandedColor,
-            indexCollapsedColor
+            placeExpandedStyle,
+            placeCollapsedStyle,
+            titleExpandedStyle,
+            titleCollapsedStyle,
+            indexExpandedStyle,
+            indexCollapsedStyle
         ) {
-            AnimatingMainTopBarColors(
-                placeExpandedColor,
-                placeCollapsedColor,
-                titleExpandedColor,
-                titleCollapsedColor,
-                indexExpandedColor,
-                indexCollapsedColor
+            AnimatingMainTopBarTextStyles(
+                placeExpandedStyle,
+                placeCollapsedStyle,
+                titleExpandedStyle,
+                titleCollapsedStyle,
+                indexExpandedStyle,
+                indexCollapsedStyle
             )
         }
-    }
 
-    @Composable
-    fun mainTopBarTextSizes(
-        titleExpandedFontSize: TextUnit,
-        titleCollapsedFontSize: TextUnit,
-        indexExpandedSp: TextUnit,
-        indexCollapsedSp: TextUnit,
-    ): MainTopBarTextSizes {
-        return remember {
-            AnimatingMainTopBarTextSizes(
-                titleExpandedFontSize,
-                titleCollapsedFontSize,
-                indexExpandedSp,
-                indexCollapsedSp,
-            )
-        }
     }
 }
 
@@ -89,26 +64,18 @@ fun MainTopBarBoxPart(
     modifier: Modifier = Modifier,
     collapsedFraction: Float,
     minHeight: Dp,
-    topBarColors: MainTopBarColors,
-    topBarTextSizes: MainTopBarTextSizes = MainTopBarDefaults.mainTopBarTextSizes(
-        titleExpandedFontSize = MaterialTheme.typography.displaySmall.fontSize,
-        titleCollapsedFontSize = MaterialTheme.typography.titleLarge.fontSize,
-        indexExpandedSp = MaterialTheme.typography.displayLarge.fontSize,
-        indexCollapsedSp = MaterialTheme.typography.titleLarge.fontSize
-    ),
-    placeContent: @Composable () -> Unit,
-    uvTitleText: String,
-    indexContent: @Composable () -> Unit
+    textStyles: MainTopBarTextStyles,
+    placeContent: @Composable (fraction: Float) -> Unit,
+    titleContent: @Composable (fraction: Float) -> Unit,
+    indexContent: @Composable (fraction: Float) -> Unit
 
 ) {
     val placeAlpha = (1f - collapsedFraction * 1.5f).coerceAtLeast(0f)
-    val colorTransitionFraction = 1f - collapsedFraction
+    val fraction = 1f - collapsedFraction
 
-    val placeTextColor by topBarColors.placeTextColor(colorTransitionFraction = colorTransitionFraction)
-    val titleTextColor by topBarColors.titleTextColor(colorTransitionFraction = colorTransitionFraction)
-    val titleFontSize by topBarTextSizes.titleTextSize(textTransitionFraction = colorTransitionFraction)
-    val indexFontSize by topBarTextSizes.indexTextSize(textTransitionFraction = colorTransitionFraction)
-
+    val placeStyle by textStyles.placeTextStyle(fraction = fraction)
+    val titleStyle by textStyles.titleTextStyle(fraction = fraction)
+    val indexStyle by textStyles.indexTextStyle(fraction = fraction)
 
     Layout(
         modifier = modifier,
@@ -116,36 +83,25 @@ fun MainTopBarBoxPart(
             Box(
                 Modifier.layoutId("placeContent")
             ) {
-                CompositionLocalProvider(
-                    LocalContentColor provides placeTextColor.copy(alpha = placeAlpha)
-                ) {
-                    placeContent()
+                ProvideTextStyle(value = placeStyle) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides placeStyle.color.copy(alpha = placeAlpha)
+                    ) {
+                        placeContent(fraction)
+                    }
                 }
             }
 
             Box(
                 Modifier.layoutId("uvTitleContent")
             ) {
-                Text(
-                    modifier = Modifier.padding(end = 16.dp),
-                    text = uvTitleText,
-                    style = MaterialTheme.typography.displaySmall,
-                    fontSize = titleFontSize,
-                    color = titleTextColor
-                )
+                ProvideTextStyle(value = titleStyle) { titleContent(fraction) }
             }
 
             Box(
                 Modifier.layoutId("indexContent")
             ) {
-
-                ProvideTextStyle(
-                    value = MaterialTheme.typography.displayLarge.copy(
-                        fontSize = indexFontSize
-                    )
-                ) {
-                    indexContent()
-                }
+                ProvideTextStyle(value = indexStyle) { indexContent(fraction) }
             }
         }
     )
@@ -199,118 +155,94 @@ fun MainTopBarBoxPart(
         }
     }
 
-
 }
 
 @Stable
-private class AnimatingMainTopBarColors(
-    private val placeExpandedColor: Color,
-    private val placeCollapsedColor: Color,
-    private val titleExpandedColor: Color,
-    private val titleCollapsedColor: Color,
-    private val indexExpandedColor: Color,
-    private val indexCollapsedColor: Color,
-) : MainTopBarColors {
+private class AnimatingMainTopBarTextStyles(
+    private val placeExpandedStyle: TextStyle,
+    private val placeCollapsedStyle: TextStyle,
+    private val titleExpandedStyle: TextStyle,
+    private val titleCollapsedStyle: TextStyle,
+    private val indexExpandedStyle: TextStyle,
+    private val indexCollapsedStyle: TextStyle
+
+) : MainTopBarTextStyles {
+
     @Composable
-    override fun placeTextColor(colorTransitionFraction: Float): State<Color> {
-        return remember(colorTransitionFraction) {
+    override fun placeTextStyle(fraction: Float): State<TextStyle> {
+        return remember(fraction) {
             derivedStateOf {
-                Color(
-                    red = (placeCollapsedColor.red + colorTransitionFraction * (placeExpandedColor.red - placeCollapsedColor.red)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    green = (placeCollapsedColor.green + colorTransitionFraction * (placeExpandedColor.green - placeCollapsedColor.green)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    blue = (placeCollapsedColor.blue + colorTransitionFraction * (placeExpandedColor.blue - placeCollapsedColor.blue)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                )
+                getFractionTextStyle(placeCollapsedStyle, placeExpandedStyle, fraction)
             }
         }
     }
 
     @Composable
-    override fun titleTextColor(colorTransitionFraction: Float): State<Color> {
-        return remember(colorTransitionFraction) {
+    override fun titleTextStyle(fraction: Float): State<TextStyle> {
+        return remember(fraction) {
             derivedStateOf {
-                Color(
-                    red = (titleCollapsedColor.red + colorTransitionFraction * (titleExpandedColor.red - titleCollapsedColor.red)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    green = (titleCollapsedColor.green + colorTransitionFraction * (titleExpandedColor.green - titleCollapsedColor.green)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    blue = (titleCollapsedColor.blue + colorTransitionFraction * (titleExpandedColor.blue - titleCollapsedColor.blue)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                )
+                getFractionTextStyle(titleCollapsedStyle, titleExpandedStyle, fraction)
             }
         }
     }
 
     @Composable
-    override fun indexTextColor(colorTransitionFraction: Float): State<Color> {
-        return remember(colorTransitionFraction) {
+    override fun indexTextStyle(fraction: Float): State<TextStyle> {
+        return remember(fraction) {
             derivedStateOf {
-                Color(
-                    red = (indexCollapsedColor.red + colorTransitionFraction * (indexExpandedColor.red - indexCollapsedColor.red)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    green = (indexCollapsedColor.green + colorTransitionFraction * (indexExpandedColor.green - indexCollapsedColor.green)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                    blue = (indexCollapsedColor.blue + colorTransitionFraction * (indexExpandedColor.blue - indexCollapsedColor.blue)).coerceIn(
-                        0f,
-                        1f
-                    ),
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalUnitApi::class)
-@Stable
-private class AnimatingMainTopBarTextSizes(
-    private val titleExpandedSp: TextUnit,
-    private val titleCollapsedSp: TextUnit,
-    private val indexExpandedSp: TextUnit,
-    private val indexCollapsedSp: TextUnit,
-) : MainTopBarTextSizes {
-
-    @Composable
-    override fun titleTextSize(textTransitionFraction: Float): State<TextUnit> {
-        return remember(textTransitionFraction) {
-            derivedStateOf {
-                TextUnit(
-                    titleCollapsedSp.value + textTransitionFraction * (titleExpandedSp.value - titleCollapsedSp.value),
-                    TextUnitType.Sp
-                )
+                getFractionTextStyle(indexCollapsedStyle, indexExpandedStyle, fraction)
             }
         }
     }
 
-    @Composable
-    override fun indexTextSize(textTransitionFraction: Float): State<TextUnit> {
-        return remember(textTransitionFraction) {
-            derivedStateOf {
-                TextUnit(
-                    indexCollapsedSp.value + textTransitionFraction * (indexExpandedSp.value - indexCollapsedSp.value),
-                    TextUnitType.Sp
-                )
-            }
-        }
-    }
+    private companion object {
 
+        private fun getFractionTextStyle(cStyle: TextStyle, eStyle: TextStyle, fraction: Float): TextStyle {
+            val style = eStyle.takeIf { fraction > 0.5 } ?: cStyle
+            return style.copy(
+                color = getFractionColor(cStyle.color, eStyle.color, fraction),
+                fontSize = getFractionTextSize(cStyle.fontSize, eStyle.fontSize, fraction),
+                fontWeight = getFractionFontWeight(cStyle.fontWeight, eStyle.fontWeight, fraction)
+            )
+        }
+
+        private fun getFractionFontWeight(
+            cFontWeight: FontWeight?,
+            eFontWeight: FontWeight?,
+            fraction: Float
+        ): FontWeight? {
+            cFontWeight ?: return null
+            eFontWeight ?: return null
+            return FontWeight(
+                getFractionValue(
+                    cFontWeight.weight.toFloat(),
+                    eFontWeight.weight.toFloat(),
+                    fraction).toInt()
+            )
+        }
+
+        @OptIn(ExperimentalUnitApi::class)
+        private fun getFractionTextSize(
+            cTextUnit: TextUnit,
+            eTextUnit: TextUnit,
+            fraction: Float
+        ): TextUnit {
+            return TextUnit(
+                getFractionValue(cTextUnit.value, eTextUnit.value, fraction), TextUnitType.Sp
+            )
+        }
+
+        private fun getFractionColor(cColor: Color, eColor: Color, fraction: Float): Color {
+            return Color(
+                red = getFractionValue(cColor.red, eColor.red, fraction),
+                green = getFractionValue(cColor.green, eColor.green, fraction),
+                blue = getFractionValue(cColor.blue, eColor.blue, fraction),
+            )
+        }
+
+        private fun getFractionValue(cValue: Float, eValue: Float, fraction: Float) =
+            cValue + fraction * (eValue - cValue)
+    }
 }
 
 
@@ -330,22 +262,29 @@ private fun Preview() {
 
     UVIndexAppTheme {
         Surface {
-
-
             MainTopBarBoxPart(
                 collapsedFraction = scrollBehavior.state.collapsedFraction,
-                topBarColors = MainTopBarDefaults.mainTopBarColors(
-                    placeExpandedColor = Color.White,
-                    titleExpandedColor = Color.White,
-                    indexExpandedColor = Color.Black
-                ),
+                textStyles = MainTopBarDefaults.mainTopBarTextStyles(
+                    placeExpandedStyle = MaterialTheme.typography.labelLarge,
+                    placeCollapsedStyle = MaterialTheme.typography.labelLarge,
+                    titleExpandedStyle = MaterialTheme.typography.displaySmall,
+                    titleCollapsedStyle = MaterialTheme.typography.titleMedium,
+                    indexExpandedStyle = MaterialTheme.typography.displayLarge,
+                    indexCollapsedStyle = MaterialTheme.typography.titleMedium,
+
+                    ),
                 minHeight = 64.dp,
                 placeContent = {
                     MainPlacePart(
                         modifier = Modifier.fillMaxWidth()
                     )
                 },
-                uvTitleText = "Экстремальный УФ",
+                titleContent = {
+                    Text(
+                        modifier = Modifier.padding(end = 16.dp),
+                        text = "Экстремальный УФ",
+                    )
+                },
                 indexContent = {
                     Text(
                         modifier = Modifier,
@@ -354,7 +293,6 @@ private fun Preview() {
                         color = Color.Black,
                     )
                 }
-
             )
         }
     }

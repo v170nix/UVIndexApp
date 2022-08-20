6fbb36c94525053e53b9dd700f2b35c601cc921a
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-08-20 15:48:40 +0300

    add google logo padding

--- app/src/main/java/uv/index/features/place/ui/composable/PlaceLocationScreen.kt
+++ app/src/main/java/uv/index/features/place/ui/composable/PlaceLocationScreen.kt
@@ -1,6 +1,9 @@
 package uv.index.features.place.ui.composable
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,7 +13,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.dp
 import uv.index.R
 import uv.index.features.place.parts.editlocation.ui.PlaceEditLocationViewModel
 import uv.index.features.place.parts.editlocation.ui.PlaceLocationContract
@@ -28,138 +30,57 @@ fun PlaceLocationScreen(
 ) {
 
     val state by viewModel.state.collectAsState()
-
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .background(MaterialTheme.colorScheme.surface)
-//    ) {
-
-        Scaffold(
-            topBar = {
-                SmallTopAppBar(
-                    modifier = Modifier.statusBarsPadding(),
-                    title = {
-                        Text(
-                            stringResource(id = R.string.place_screen_title_select_location)
-                        )
-                    },
-                    colors = TopAppBarDefaults.smallTopAppBarColors(
-                        containerColor = Color.Transparent
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                modifier = Modifier.statusBarsPadding(),
+                title = {
+                    Text(
+                        stringResource(id = R.string.place_screen_title_select_location)
                     )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Transparent
                 )
-            },
-            bottomBar = {
-                PlaceWizardBottomBarComponent(
-                    isEnableNextStep = state.nextStepIsAvailable,
-                    isShowNextStep = true,
-                    previousName = stringResource(R.string.place_navigation_bottom_bar_back),
-                    nextName = stringResource(R.string.place_navigation_bottom_bar_next),
-                    onPreviousClick = {
-                        viewModel.doEvent(PlaceLocationContract.Event.ClearData)
-                        onNavigateBackStack()
-                    },
-                    onNextClick = {
-                        viewModel.doEvent(PlaceLocationContract.Event.Submit)
-                    }
-                )
-            }
-        ) { paddingValues ->
-            PlaceEditPositionSection(
-                modifier = Modifier.fillMaxSize(),
-                logoOffset = DpOffset(
-                    Dimens.grid_2 + paddingValues.calculateLeftPadding(LocalLayoutDirection.current),
-                    72.dp
-                            + paddingValues.calculateBottomPadding()
-                            + WindowInsets.navigationBars.asPaddingValues()
-                        .calculateBottomPadding()
-                ),
-                eventHandler = viewModel,
-                state = state,
-                effectFlow = viewModel.effect,
-                onTimeZonePart = {}
-//                    editPositionViewMapBridger!!
             )
-
-            Column(
-                modifier = Modifier.padding(paddingValues)
-            ) {
-//                PlaceTopAppBarComponent(
-//                    title = stringResource(id = androidx.compose.foundation.layout.R.string.place_screen_title_select_location),
-//                    isBlackColor = true,
-//                    onNavigateBack = onNavigateBackStack
-//                )
-
-                InputLocationBoxComponent(
-                    state = state.inputState,
-                    eventHandler = viewModel,
-                    onPreviousClick = onNavigateBackStack
-                )
-            }
-
-
-
-//            PlaceWizardBottomBarComponent(
-//                Modifier
-//                    .align(Alignment.BottomCenter)
-//                    .navigationBarsPadding(),
-//                state.nextStepIsAvailable,
-//                true,
-//                stringResource(androidx.compose.foundation.layout.R.string.place_navigation_bottom_bar_back),
-//                stringResource(androidx.compose.foundation.layout.R.string.place_navigation_bottom_bar_next),
-//                buttonShape = MaterialTheme.shapes.small,
-//                onPreviousClick = {
-//                    editPositionViewModel.doEvent(PlaceEditPositionContract.Event.ClearData)
-//                    onNavigateBackStack()
-//                },
-//                onNextClick = {
-//                    editPositionViewModel.doEvent(PlaceEditPositionContract.Event.Submit)
-//                }
-//            )
+        },
+        bottomBar = {
+            PlaceWizardBottomBarComponent(
+                isEnableNextStep = state.nextStepIsAvailable,
+                isShowNextStep = true,
+                previousName = stringResource(R.string.place_navigation_bottom_bar_back),
+                nextName = stringResource(R.string.place_navigation_bottom_bar_next),
+                onPreviousClick = {
+                    viewModel.doEvent(PlaceLocationContract.Event.ClearData)
+                    onNavigateBackStack()
+                },
+                onNextClick = {
+                    viewModel.doEvent(PlaceLocationContract.Event.Submit)
+                }
+            )
         }
+    ) { paddingValues ->
 
-//        Box(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(horizontal = 16.dp)
-//                .navigationBarsPadding()
-//        ) {
-//
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .align(Alignment.BottomCenter)
-//                    .padding(bottom = 32.dp)
-//            ) {
-//
-//                ExtendedFloatingActionButton(
-//                    modifier = Modifier.weight(1f),
-//                    content = {
-//                        Text(
-//                            text = "Back"
-//                        )
-//                    },
-//                    onClick = onNavigateBackStack
-//                )
-//
-//                Spacer(modifier = Modifier.width(16.dp))
-//
-//                Row(
-//                    modifier = Modifier
-//                        .weight(1f),
-//                    horizontalArrangement = Arrangement.End
-//                ) {
-//
-//                    PlaceListAddButton(
-//                        isLockAddPlace = false,
-//                        onAddPlace = {
-////                            viewModel.doEvent(PlaceListContract.Event.AddPlace)
-//                        }
-//                    )
-//                }
-//
-//            }
-//
-//        }
-//    }
+        PlaceEditPositionSection(
+            modifier = Modifier.fillMaxSize(),
+            logoOffset = DpOffset(
+                Dimens.grid_2 + paddingValues.calculateLeftPadding(LocalLayoutDirection.current),
+                Dimens.grid_1 + paddingValues.calculateBottomPadding()
+            ),
+            eventHandler = viewModel,
+            state = state,
+            effectFlow = viewModel.effect,
+            onTimeZonePart = {}
+        )
+
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            InputLocationBoxComponent(
+                state = state.inputState,
+                eventHandler = viewModel,
+                onPreviousClick = onNavigateBackStack
+            )
+        }
+    }
 }

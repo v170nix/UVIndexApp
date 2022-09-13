c216cb48441938008120761c3c817d07f096d58d
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-09-13 22:59:54 +0300

    add navigation bar strings

--- app/src/main/java/uv/index/navigation/AppNavigationBar.kt
+++ app/src/main/java/uv/index/navigation/AppNavigationBar.kt
@@ -1,5 +1,6 @@
 package uv.index.navigation
 
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
@@ -11,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import uv.index.LocalAppState
+import uv.index.R
 
 @Composable
 fun AppNavigationBar() {
@@ -24,10 +27,10 @@ fun AppNavigationBar() {
     val actions = LocalAppState.current.actions
 
     NavigationBar {
-        items.forEachIndexed { index, item ->
+        items.forEach { item ->
             NavigationBarItem(
                 icon = { Icon(imageVector = item.icon, contentDescription = null) },
-                label = { Text(item.name) },
+                label = { Text( stringResource(id = item.nameId)) },
                 selected = currentDestination?.hierarchy?.any {
                     when (item) {
                         is NavigationItems.Notification -> false
@@ -38,16 +41,7 @@ fun AppNavigationBar() {
                     when (item) {
                         is NavigationItems.Notification -> {}
                         is NavigationItems.Screen -> {
-
                             actions.bottomBarNavigateTo(item.data)
-//
-//                            navController.navigate(item.data.route) {
-//                                popUpTo(navController.graph.findStartDestination().id) {
-//                                    saveState = true
-//                                }
-//                                launchSingleTop = true
-//                                restoreState = true
-//                            }
                         }
                     }
 
@@ -57,23 +51,23 @@ fun AppNavigationBar() {
     }
 }
 
-private sealed class NavigationItems(val icon: ImageVector, val name: String) {
+private sealed class NavigationItems(val icon: ImageVector, @StringRes val nameId: Int) {
     class Screen(
         val data: AppScreen,
         icon: ImageVector,
-        name: String
-    ) : NavigationItems(icon, name)
+        @StringRes nameId: Int
+    ) : NavigationItems(icon, nameId)
 
     class Notification(
         val isOn: Boolean,
         icon: ImageVector,
-        name: String
-    ) : NavigationItems(icon, name)
+        @StringRes nameId: Int
+    ) : NavigationItems(icon, nameId)
 }
 
 private val items = listOf<NavigationItems>(
-    NavigationItems.Screen(AppScreen.Main, Icons.Filled.Home, "В начало"),
-    NavigationItems.Screen(AppScreen.SkinType, Icons.Filled.Person, "Тип кожи"),
- //   NavigationItems.Notification(false, Icons.Filled.Notifications, "Извещения"),
-    NavigationItems.Screen(AppScreen.More, Icons.Filled.Menu, "Далее")
+    NavigationItems.Screen(AppScreen.Main, Icons.Filled.Home, R.string.navigation_bar_main_screen_title),
+    NavigationItems.Screen(AppScreen.SkinType, Icons.Filled.Person, R.string.navigation_bar_skin_screen_title),
+    //   NavigationItems.Notification(false, Icons.Filled.Notifications, "Извещения"),
+    NavigationItems.Screen(AppScreen.More, Icons.Filled.Menu, R.string.navigation_bar_more_screen_title)
 )
